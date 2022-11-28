dbbe0260ea21add0679324289b623a29bacb423c
Adam Marut <adam.marut@icloud.com>
2022-11-28 20:24:56 +0100

    Obsługa kamery przez AVFoundation

--- Shop shelf scanner/Controller/ViewController.swift
+++ Shop shelf scanner/Controller/ViewController.swift
@@ -8,18 +8,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     let imagePicker = UIImagePickerController()
     var acc = AccelerometerHandler(updateInterval: 1.0/60.0)
     @IBOutlet weak var emailTxtBox: UITextField!
+    @IBOutlet weak var overlayPhotoImageView: UIImageView!
     @IBOutlet weak var secondsStepper: UIStepper!
     @IBOutlet weak var secondsLabel: UITextField!
     @IBOutlet weak var imageBox: UIImageView!
     
+    
+    @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var lastPhotoImageView: UIImageView!
+    var previewView : UIView!
+
+    //Camera Capture requiered properties
+      var videoDataOutput: AVCaptureVideoDataOutput!
+      var videoDataOutputQueue: DispatchQueue!
+      var previewLayer:AVCaptureVideoPreviewLayer!
+      var captureDevice : AVCaptureDevice!
+      let session = AVCaptureSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
         imagePicker.delegate = self
+       // previewView = UIView(frame: CGRect(x: 0,
+//                                                   y: 0,
+//                                                   width: UIScreen.main.bounds.size.width,
+//                                                   height: UIScreen.main.bounds.size.height))
+       // previewView.contentMode = UIView.ContentMode.scaleAspectFit
+      //  view.addSubview(previewView)
         
-        
+        self.setupAVCapture()
+
        // self.modalPresentationStyle = .fullScreen
         // Do any additional setup after loading the view.
     }
@@ -54,6 +72,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         present(imagePicker, animated: true, completion: nil)
         if (imageBox.image != nil){
             lastPhotoImageView.image = imageBox.image
+            let cropped = cropToBounds(image: imageBox.image!, width: 130.0, height: 649.0)
+            overlayPhotoImageView.image = cropped.alpha(0.5)
+            
         }
     }
     
@@ -97,16 +118,129 @@ func getImageFromBase64(stringData:String)->UIImage?{
     return nil
 }
 
-class VideoHelper {
-    
-    static func startMediaBrowser(delegate: UIViewController & UINavigationControllerDelegate & UIImagePickerControllerDelegate, sourceType: UIImagePickerController.SourceType) {
-        guard UIImagePickerController.isSourceTypeAvailable(sourceType) else { return }
-        
-        let mediaUI = UIImagePickerController()
-        mediaUI.sourceType = sourceType
-        mediaUI.mediaTypes = [UTType.movie.identifier]
-        mediaUI.allowsEditing = true
-        mediaUI.delegate = delegate
-        delegate.present(mediaUI, animated: true, completion: nil)
+
+func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
+
+    let contextImage: UIImage = UIImage(cgImage: image.cgImage!)
+
+    let contextSize: CGSize = contextImage.size
+
+    var posX: CGFloat = 0.0
+    var posY: CGFloat = 0.0
+    var cgwidth: CGFloat = CGFloat(width)
+    var cgheight: CGFloat = CGFloat(height)
+
+    // See what size is longer and create the center off of that
+    if contextSize.width > contextSize.height {
+        posX = ((contextSize.width - contextSize.height) / 2)
+        posY = 0
+        cgwidth = contextSize.height
+        cgheight = contextSize.height
+    } else {
+        posX = 0
+        posY = ((contextSize.height - contextSize.width) / 2)
+        cgwidth = contextSize.width
+        cgheight = contextSize.width
     }
+
+    let rect: CGRect = CGRectMake(posX, posY, cgwidth, cgheight)
+
+    // Create bitmap image from context using the rect
+    let imageRef = contextImage.cgImage!.cropping(to: rect)
+
+    // Create a new image based on the imageRef and rotate back to the original orientation
+    let image: UIImage = UIImage(cgImage: imageRef!, scale: image.scale, orientation: image.imageOrientation)
+
+    return image
+}
+
+//
+//class VideoHelper {
+//
+//    static func startMediaBrowser(delegate: UIViewController & UINavigationControllerDelegate & UIImagePickerControllerDelegate, sourceType: UIImagePickerController.SourceType) {
+//        guard UIImagePickerController.isSourceTypeAvailable(sourceType) else { return }
+//
+//        let mediaUI = UIImagePickerController()
+//        mediaUI.sourceType = sourceType
+//        mediaUI.mediaTypes = [UTType.movie.identifier]
+//        mediaUI.allowsEditing = true
+//        mediaUI.delegate = delegate
+//        delegate.present(mediaUI, animated: true, completion: nil)
+//    }
+//}
+
+extension UIImage{
+    
+    func alpha(_ value:CGFloat) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, scale)
+        draw(at: CGPoint.zero, blendMode: .normal, alpha: value)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+        
+    }
+}
+
+extension ViewController:  AVCaptureVideoDataOutputSampleBufferDelegate{
+     func setupAVCapture(){
+         session.sessionPreset = AVCaptureSession.Preset.hd4K3840x2160
+        guard let device = AVCaptureDevice
+        .default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
+                 for: .video,
+                 position: AVCaptureDevice.Position.back) else {
+                            return
+        }
+        captureDevice = device
+        beginSession()
+    }
+
+    func beginSession(){
+        var deviceInput: AVCaptureDeviceInput!
+
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            guard deviceInput != nil else {
+                print("error: cant get deviceInput")
+                return
+            }
+
+            if self.session.canAddInput(deviceInput){
+                self.session.addInput(deviceInput)
+            }
+
+            videoDataOutput = AVCaptureVideoDataOutput()
+            videoDataOutput.alwaysDiscardsLateVideoFrames=true
+            videoDataOutputQueue = DispatchQueue(label: "VideoDataOutputQueue")
+            videoDataOutput.setSampleBufferDelegate(self, queue:self.videoDataOutputQueue)
+
+            if session.canAddOutput(self.videoDataOutput){
+                session.addOutput(self.videoDataOutput)
+            }
+
+            videoDataOutput.connection(with: .video)?.isEnabled = true
+
+            previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+            if (self.cameraView != nil){
+                let rootLayer :CALayer = self.cameraView.layer
+                rootLayer.masksToBounds=true
+                previewLayer.frame = rootLayer.bounds
+                rootLayer.addSublayer(self.previewLayer)
+                session.startRunning()
+            }
+        } catch let error as NSError {
+            deviceInput = nil
+            print("error: \(error.localizedDescription)")
+        }
+    }
+
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // do stuff here
+    }
+
+    // clean up AVCapture
+    func stopCamera(){
+        session.stopRunning()
+    }
+
 }
